a1c6489b943c8cf2097990b2a6d18ca9b8d2e7e8
Trebushnikov Dmitry <TrebushnikovDA@yandex.ru>
2022-04-10 15:50:16 +0300

    HW 2 is almost done

---
+++ ugly_ask/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for ugly_ask project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ugly_ask.settings')
+
+application = get_asgi_application()
